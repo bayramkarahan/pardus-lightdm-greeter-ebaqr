9ff87db84f6f58ed0082efd66ace6d0dfd399644
bayramkarahan <bayramk@gmail.com>
2024-02-21 13:39:24 +0300

    tets

--- ebaonline.py
+++ ebaonline.py
@@ -9,54 +9,54 @@ class EbaonlineWidget(Gtk.Box):
 
     def __init__(self):
         super().__init__()
-        self.__web=WebKit2.WebView()
-        self.__web.set_size_request(400,700)
-        self.__web2=WebKit2.WebView()
-        self.__web.load_uri("https://giris.eba.gov.tr/EBA_GIRIS/studentQrcode.jsp")
-        self.__web.connect("load-changed",self.__load_event)
-        self.__web.set_zoom_level(self.__web.get_zoom_level() - 0.2)
-        self.__web2.connect("load-changed",self.__load_event2)
-        self.__web.set_sensitive(False)
-        self.data_action = None
-        s = Gtk.ScrolledWindow()
-        s.set_size_request(400,700)
-        s.add(self.__web)
-        self.add(s)
+        self.__webonline=WebKit2.WebView()
+        self.__webonline.set_size_request(400,700)
+        self.__webonline2=WebKit2.WebView()
+        self.__webonline.load_uri("https://giris.eba.gov.tr/EBA_GIRIS/studentQrcode.jsp")
+        self.__webonline.connect("load-changed",self.__load_eventonline)
+        self.__webonline.set_zoom_level(self.__webonline.get_zoom_level() - 0.2)
+        self.__webonline2.connect("load-changed",self.__load_event2online)
+        self.__webonline.set_sensitive(False)
+        self.data_actiononline = None
+        self.s = Gtk.ScrolledWindow()
+        self.s.set_size_request(400,700)
+        self.s.add(self.__webonline)
+        self.add(self.s)
         self.show_all()
 
     def refresh(self,widget=None):
-        self.__web.get_website_data_manager().clear(WebKit2.WebsiteDataTypes.ALL,0,None,None,None)
+        self.__webonline.get_website_data_manager().clear(WebKit2.WebsiteDataTypes.ALL,0,None,None,None)
         self.show_all()
-        self.__web.load_uri("https://giris.eba.gov.tr/EBA_GIRIS/studentQrcode.jsp")
+        self.__webonline.load_uri("https://giris.eba.gov.tr/EBA_GIRIS/studentQrcode.jsp")
 
-    def __load_event(self,webkit,event):
+    def __load_eventonline(self,webkit,event):
         link=webkit.get_uri()
         if "studentQrcode" in link:
             return
         elif "ders.eba.gov.tr" not in link:
-            self.__web.load_uri("https://giris.eba.gov.tr/EBA_GIRIS/studentQrcode.jsp")
+            self.__webonline.load_uri("https://giris.eba.gov.tr/EBA_GIRIS/studentQrcode.jsp")
             return
-        self.__web2.load_uri("https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//home/user/getuserinfo")
+        self.__web2online.load_uri("https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//home/user/getuserinfo")
 
-    def __load_event2(self,webkit,event):
+    def __load_event2online(self,webkit,event):
         link=webkit.get_uri()
         resource = webkit.get_main_resource()
         if resource:
-            resource.get_data(None,self.__response_data,None)
+            resource.get_data(None,self.__response_dataonline,None)
 
-    def __response_data(self,resource,result,data=None):
+    def __response_dataonline(self,resource,result,data=None):
         html = resource.get_data_finish(result)
         data = html.decode("utf-8")
-        if self.data_action:
-            self.data_action(data)
+        if self.data_actiononline:
+            self.data_actiononline(data)
         self.hide()
 
 if __name__ == "__main__":
-    def write(data):
+    def writeonline(data):
         print(data)
-    win1 = Gtk.Window()
-    q1 = EbaonlineWidget()
-    q1.data_action = write
-    win1.add(q1)
-    win1.show_all()
+    win1online = Gtk.Window()
+    q1online = EbaonlineWidget()
+    q1online.data_actiononline = writeonline
+    win1online.add(q1online)
+    win1online.show_all()
     Gtk.main()
